f5a050f2db022b4b6cdcddf9f84982b976ca4f46
200727379 <rampedilesley@gmail.com>
2021-04-30 10:04:44 -0700

    initial commit

---
+++ dictionary_exists.py
@@ -0,0 +1,23 @@
+#Author: Mothapo Rampedi Lesley
+#Email: rampedilesley@gmail.com
+#30 April 2021
+
+"""
+Write a Python script to check whether a given key already exists in a dictionary
+
+"""
+#define a dictonary called customer
+customer = {
+    "name" : "Mothapo Rampedi",
+    "age" : 33,
+    "language" : "Python"
+   
+}
+print(customer.keys())
+new_key = input("Enter the a key: ")
+
+if new_key in customer:
+    print("Key exist")
+    
+else:
+     print("Key does not exist")
